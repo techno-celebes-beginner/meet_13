e7eebe37123031207ad27abb985d3badbcbbdcc5
Techno Celebes <cvtechnocelebes@gmail.com>
2020-07-02 10:08:21 +0800

    second commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,91 @@
+@extends("layout.master")
+
+@section('content')
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <div class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1 class="m-0 text-dark">Dashboard</h1>
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                    <li class="breadcrumb-item active">Dashboard v1</li>
+                                </ol>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <!-- /.container-fluid -->
+                </div>
+                <!-- /.content-header -->
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="container-fluid">
+                        <!-- Small boxes (Stat box) -->
+                        <div class="row">
+							<table class="table table-bordered">
+							  <thead>                  
+							    <tr>
+							      <th style="width: 10px">#</th>
+							      <th>Task</th>
+							      <th>Progress</th>
+							      <th style="width: 40px">Label</th>
+							    </tr>
+							  </thead>
+							  <tbody>
+							    <tr>
+							      <td>1.</td>
+							      <td>Update software</td>
+							      <td>
+							        <div class="progress progress-xs">
+							          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+							        </div>
+							      </td>
+							      <td><span class="badge bg-danger">55%</span></td>
+							    </tr>
+							    <tr>
+							      <td>2.</td>
+							      <td>Clean database</td>
+							      <td>
+							        <div class="progress progress-xs">
+							          <div class="progress-bar bg-warning" style="width: 70%"></div>
+							        </div>
+							      </td>
+							      <td><span class="badge bg-warning">70%</span></td>
+							    </tr>
+							    <tr>
+							      <td>3.</td>
+							      <td>Cron job running</td>
+							      <td>
+							        <div class="progress progress-xs progress-striped active">
+							          <div class="progress-bar bg-primary" style="width: 30%"></div>
+							        </div>
+							      </td>
+							      <td><span class="badge bg-primary">30%</span></td>
+							    </tr>
+							    <tr>
+							      <td>4.</td>
+							      <td>Fix and squish bugs</td>
+							      <td>
+							        <div class="progress progress-xs progress-striped active">
+							          <div class="progress-bar bg-success" style="width: 90%"></div>
+							        </div>
+							      </td>
+							      <td><span class="badge bg-success">90%</span></td>
+							    </tr>
+							  </tbody>
+							</table>
+                        </div>
+                        <!-- /.row (main row) -->
+                    </div>
+                    <!-- /.container-fluid -->
+                </section>
+                <!-- /.content -->
+            </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "HomeController@index");
+Route::get('/data-table',"HomeController@datatable");
